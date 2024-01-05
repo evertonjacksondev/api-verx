486fd99c9ad7bf54ec1b20d950b963924eb7945f
Everton Jackson <dev.evertonjackson@gmail.com>
2024-01-05 16:58:04 -0300

    feat: add lib pg to connect with postgreeSql

---
+++ start/routes.ts
@@ -0,0 +1,23 @@
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/v1', async () => {
+  return { hello: 'world' }
+})
+
+Route.post('/v1', async () => {
+  return { hello: 'world' }
+})
+
+Route.put('/v1', async () => {
+  return { hello: 'world' }
+})
+
+Route.patch('/v1', async () => {
+  return { hello: 'world' }
+})
+
+Route.delete('/v1', async () => {
+  return { hello: 'world' }
+})
+
