216860651d7f3516a1074985ed9f1bd676830c4b
Everton Jackson <dev.evertonjackson@gmail.com>
2024-01-09 10:25:01 -0300

    feat: created api to post user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,14 +1,29 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-import User from "App/Models/User"
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 
 export default class UsersController {
 
-    async createUser({ request }) {
-        const dataToCreate = request.only(['name','city','UF'])
-        console.log(dataToCreate)
-        return await User.create(dataToCreate)
+    async createUser({ request, response }: HttpContextContract) {
+        try {
+            const { name, city, UF } = request.only(['name', 'city', 'UF']);
+
+            const user = await User.create({ city, name, UF })
+            return response.status(201).json(user);
+        } catch (error) {
+            return response.status(500).json({ error: error.message });
+        }
+    }
+
+
+    async list({ response }: HttpContextContract) {
+        try {
+
+            const user = await User.all()
+            return response.status(200).json(user);
+        } catch (error) {
+            return response.status(500).json({ error: error.message });
+        }
     }
 
 }
